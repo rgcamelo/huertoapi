a0b94ab573b8908d262187c6c02372a65a9b270a
Rafael Camelo <rafaelgcamelo@gmail.com>
2021-04-18 02:02:39 -0500

    Update SeedController.php

--- app/Http/Controllers/Seed/SeedController.php
+++ app/Http/Controllers/Seed/SeedController.php
@@ -24,7 +24,7 @@ class SeedController extends ApiController
     public function index()
     {
         $seeds = Seed::all();
-
+        $seeds = $seeds->sortBy('name');
         return $this->showAll($seeds);
     }
 
