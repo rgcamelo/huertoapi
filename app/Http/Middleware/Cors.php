70e8bcf703822490d5149a8aab1c522f4d252581
Rafael Camelo <rafaelgcamelo@gmail.com>
2021-04-17 22:47:50 -0500

    cors update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin','*')
-        ->header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers','Content-Type, Authorization');
-    }
+        return $next($request)
+           //Url a la que se le dará acceso en las peticiones
+          ->header("Access-Control-Allow-Origin", "https://huertoapi.herokuapp.com/")
+          //Métodos que a los que se da acceso
+          ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+          //Headers de la petición
+          ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+      }
 }
