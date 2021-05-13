a1f2215d4ee9f1560c58ab84e19c79e355c1dba0
Rafael Camelo <rafaelgcamelo@gmail.com>
2021-05-13 13:38:14 -0500

    Update

---
+++ app/Observers/SeedObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Seed;
+
+class SeedObserver
+{
+    /**
+     * Handle the Seed "created" event.
+     *
+     * @param  \App\Models\Seed  $seed
+     * @return void
+     */
+    public function created(Seed $seed)
+    {
+        //
+    }
+
+    /**
+     * Handle the Seed "updated" event.
+     *
+     * @param  \App\Models\Seed  $seed
+     * @return void
+     */
+    public function updated(Seed $seed)
+    {
+        //
+    }
+
+    /**
+     * Handle the Seed "deleted" event.
+     *
+     * @param  \App\Models\Seed  $seed
+     * @return void
+     */
+    public function deleted(Seed $seed)
+    {
+        $seed->plants()->delete();
+    }
+
+    /**
+     * Handle the Seed "restored" event.
+     *
+     * @param  \App\Models\Seed  $seed
+     * @return void
+     */
+    public function restored(Seed $seed)
+    {
+        //
+    }
+
+    /**
+     * Handle the Seed "force deleted" event.
+     *
+     * @param  \App\Models\Seed  $seed
+     * @return void
+     */
+    public function forceDeleted(Seed $seed)
+    {
+        //
+    }
+}
